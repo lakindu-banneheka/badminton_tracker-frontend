248ff9e3f12189e333241585f005e76ca80e22ad
lakindu-banneheka <lakindubannaheka@gmail.com>
2024-03-02 00:54:29 +0530

    remove counter .js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import { Navigate } from "react-router-dom";
